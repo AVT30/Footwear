da2b1eef6bdaf6654dd8caa300df23fac3327f37
AVT30 <123390487+AVT30@users.noreply.github.com>
2023-02-09 11:04:46 +0100

    Version 1.2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('utilisateurs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nom');
+            $table->string('prenom');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('genre');
+            $table->boolean('role');
             $table->rememberToken();
             $table->timestamps();
         });
